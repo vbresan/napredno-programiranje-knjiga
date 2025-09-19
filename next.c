a3a28c61fc2e430d30da92359a9964905e96dfff
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-09-19 10:16:01 +0200

    Create next.c

---
+++ next.c
@@ -0,0 +1,16 @@
+/*
+Napravi program koji pronalazi i ispisuje poteze potrebne da bi se posložila 
+slagalica 2x3 ili ispisuje da ne postoji način. Slagalica 2x3 sastoji se od 
+brojeva od 1 do 6. Svaki broj se pojavljuje jednom i brojevi su posloženi u 
+matricu od 2 retka i 3 stupca. Cilj je brojeve, korištenjem nekog broja poteza, 
+posložiti u oblik:
+
+123 
+456
+
+Pritom se smiju koristiti samo potezi koji zamjenjuju dva broja na poljima 
+koja dijele zajedničku stranicu. Postoji ukupno 7 takvih poteza. To su zamjena 
+dvaju brojeva iz istog stupca ili zamjena dvaju susjednih brojeva u istom 
+retku. Ulazni podaci su 6 brojeva koji predstavljaju sadržaj slagalice.
+*/
+
