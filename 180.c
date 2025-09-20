9df0cf5b89fe192e885695aff9e92fbc30be8e2c
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-09-20 09:52:25 +0200

    Opis novog zadatka

---
+++ 180.c
@@ -0,0 +1,12 @@
+/*
+Unose se dva broja n i m (1 <= n,m <= 1000) koji predstavljaju dimenzije 
+tlocrta grada koji se unosi. Zatim se unosi n linija s po m znakova koji 
+predstavljaju izgled grada. Svaki uneseni znak može biti '.', 'X, 'o' 
+ili '-'. '.' predstavlja prolazan prostor tj. cestu, 'X' predstavlja 
+neprolazan prostor tj. zgradu. 'o' je burek, a '-'je Gladni. Postoji samo 
+jedan Gladni. Gladni se može kretati gore, dolje, lijevo i desno kroz grad. 
+Ispiši koliko najviše bureka Gladni može pojesti i koliko najmanje koraka 
+treba napraviti tako da može od početne lokacije doći do svakog bureka koji 
+može pojesti.
+*/
+
