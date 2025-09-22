b311f8fa4adb2f11164f5381889656f462ad7382
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-09-22 15:32:00 +0200

    Adding new problem descriptions

---
+++ brojevi.c
@@ -0,0 +1,10 @@
+/*
+Brojevi su posloženi u krug i dodaju se frizbijem. Unosi se n (1 <= n <= 1000)
+i zatim n prirodnih brojeva manjih od 10^9. Drugi uneseni broj je desno od 
+prvoga, treći uneseni broj je desno od drugog, itd. Prvi uneseni broj je desno
+od zadnjega. Na početku prvi broj ima frizbi. Ako je prvi broj pet, onda će on
+izići iz kruga i dodati frizbi petom zdesna. Onaj tko dobije frizbi će izići 
+iz kruga i dodati ga x-tom zdesna, ako mu je vrijednost x. Brojevi izlaze iz 
+kruga i bacaju frizbi. Ispiši brojeve onim redom kojim su izlazili iz kruga.
+*/
+
---
+++ djeca.c
@@ -0,0 +1,11 @@
+/*
+Djeca su se uhvatila za ruke u lanac i trče livadom. Kako je netko brži, a 
+netko sporiji, događa se da brža djeca vuku sporiju. U lanac se uhvatilo n 
+(1 <= n <= 100000) djece tako da svako dijete drži za ruke dvoje djece koji 
+su pored njega (osim prvog i zadnjeg djeteta u lancu, kojima je jedna ruka 
+slobodna). Svako dijete ima svoju maksimalnu brzinu x_i (0 <= x_i <= 1000) 
+kojom može trčati i dio brzine y_i, (1 <= y_i <= 1000) koju gubi za svako 
+dijete koje ga vuče ili koga on vuče (jer postaje nestabilno i može pasti). 
+Djeca se mogu međusobno vući tako što dio svoje snage utroše na povlačenje 
+drugog djeteta. Koliko najbrže lanac djece može trčati?
+*/
