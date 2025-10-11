44040a9b8618808d736f5faa44ece0e8237f1de5
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-10-11 17:08:28 +0200

    Adding new problem

---
+++ 06.03_s235_tvornica.cpp
@@ -0,0 +1,14 @@
+/*
+
+Tvornica pravokutnog oblika sastoji se od n (1 <= n <= 1000) redova i m 
+(1 <= m <= 1000) stupaca. Ukupno n * m dijelova. Svaki dio tvornice ima 
+zidove s nekih strana. Unosi se n i m i zatim n redova od po m brojeva. 
+Svaki broj od 0 do 15 opisuje jedan dio tvornice. Binarni zapis broja 
+opisuje s kojih strana tog dijela tvornice se nalaze zidovi. Ako je dan 
+npr. broj 10, onda je njegov binarni zapis 1010, što znači da ima zid sa 
+sjevera, nema s istoka, ima s juga i nema sa zapada. Opis će biti takav 
+da tvornica ima zidove s vanjske strane. Ispiši za svaku prostoriju njenu 
+veličinu. Ispiši prostorije od veće prema manjoj.
+
+*/
+
