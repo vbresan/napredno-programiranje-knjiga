b684b0479f280fdc85f3c337484176bfe96665d9
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-10-12 16:39:58 +0200

    Adding new problem and sample input

---
+++ 06.04_s240_gradovi.cpp
@@ -0,0 +1,14 @@
+/*
+
+Pronađi najkraći put od zadanog grada do svih ostalih i ispiši udaljenosti 
+od zadanog grada do svih gradova poredanih po njihovu indeksu. Ukupno postoji 
+n (1 <= n <= 1000) gradova i m (1 <= m <= n*n) cesta. Gradovi su 
+0-indeksirani. Ulazni podaci su n, m i x. x je početni grad. Zatim slijedi m 
+puta po tri broja a, b i c (0 <= a,b < n), (1 <= c <= 1000) koji opisuju 
+dvosmjernu cestu dužine c koja povezuje gradove a i b. Postojat će put od 
+svakog grada do svakog drugog.
+
+out:
+0 10 13 4 12 10 6 9 20
+
+*/
