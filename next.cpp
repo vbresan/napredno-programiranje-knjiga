3d843a2f0aa1d7522b8244de3b4f227a932a96be
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-10-15 19:11:34 +0200

    New problem description and input

---
+++ next.cpp
@@ -0,0 +1,14 @@
+/*
+
+Unosi se dva broja: n (1 <= n <= 1000) im (1 <= m <= 1000), a zatim n 
+linija dužine m zna- kova. Svaki znak je '.' ili 'x' ili '#! Ti znakovi 
+opisuju tlocrt krajolika. '.' predstavlja prohodnu travu i pri prelasku 
+preko tog polja ne trošimo vrijeme. 'x' predstavlja šumu kroz koju nam 
+treba jedan sat, dok je stijena kroz koju ne možemo proći. Koliko sati 
+nam treba da bi- smo došli od gornjeg lijevog polja do donjeg desnog 
+najbržim putem? S pojedinog polja možemo prijeći na bilo koje od 
+susjednih 8 polja (ako je prohodno). Donje desno i gornje lijevo polje 
+bit će trava i moći će se doći od jednog do drugog.
+
+
+*/
