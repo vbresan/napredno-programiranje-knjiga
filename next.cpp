821e7bfe96ed8fe1c184f49d89e6de5a51b71dde
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-10-17 13:49:19 +0200

    Adding input and problem description

---
+++ next.cpp
@@ -0,0 +1,12 @@
+/*
+
+Mala država želi povezati svoje gradove tako da izgradi ceste između njih. 
+Cilj je da svi gradovi budu povezani uz što manje troškove pri izgradnji 
+cesta. Ukupno postoji n gradova i postoji plan izgradnje za m cesta od 
+kojih treba neke izabrati. Ulazni podaci u prvoj liniji imaju brojeve n 
+(1 <= n <= 1000) i m (1 <= m <= n^2). Zatim se nalazi m puta po tri broja 
+a, b (0 <= a,b < n) i c (1 <= c <= 1000) koji opisuju plan za izgradnju 
+dvosmjerne ceste između gradova a i b koja košta c novaca. Ispiši najmanji 
+potreban trošak za povezivanje svih n gradova. (Gradovi će se moći povezati.)
+
+*/
