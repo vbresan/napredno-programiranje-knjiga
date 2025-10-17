ed12d42d6174c17363cc3e94ba4ab4f74459233e
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-10-17 06:01:48 +0200

    Solution implementation

--- 06.07_s252_krajolik.cpp
+++ 06.07_s252_krajolik.cpp
@@ -13,3 +13,60 @@ bit će trava i moći će se doći od jednog do drugog.
 output: 4
 
 */
+
+#include <iostream>
+#include <list>
+#include <utility>
+#include <vector>
+using namespace std;
+
+const struct Step {
+  int x, y;
+} steps[] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1},
+             {1, 1},   {1, 0},  {1, -1}, {0, -1}};
+
+int main() {
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<string> map(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> map[i];
+  }
+
+  list<pair<pair<int, int>, int>> nodes; // ((x, y), distance)
+  nodes.emplace_back(make_pair(0, 0), 0);
+
+  while (!nodes.empty()) {
+
+    const auto [coordinates, distance] = nodes.back();
+    const auto [x, y] = coordinates;
+    nodes.pop_back();
+
+    if (x == n - 1 && y == m - 1) {
+      cout << distance << endl;
+      break;
+    }
+
+    map[x][y] = '#'; // mark as visited
+    for (int i = 0; i < 8; ++i) {
+
+      const int neighbor_x = x + steps[i].x;
+      const int neighbor_y = y + steps[i].y;
+      if (map[neighbor_x][neighbor_y] == '#' ||
+          (neighbor_x < 0 || neighbor_x >= n) ||
+          (neighbor_y < 0 || neighbor_y >= m)) {
+        continue;
+      }
+
+      if (map[neighbor_x][neighbor_y] == 'x') {
+        nodes.emplace_front(make_pair(neighbor_x, neighbor_y), distance + 1);
+      } else {
+        nodes.emplace_back(make_pair(neighbor_x, neighbor_y), distance);
+      }
+    }
+  }
+
+  return 0;
+}
