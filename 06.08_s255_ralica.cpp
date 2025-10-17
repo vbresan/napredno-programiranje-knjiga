ad40f49777bf6ef5f5996593c1eeba5735831111
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-10-17 07:30:00 +0200

    New problem description and input

---
+++ 06.08_s255_ralica.cpp
@@ -0,0 +1,14 @@
+/*
+
+Pao je snijeg koji ralica mora počistiti sa svih međugradskih cesta. 
+Postoji n gradova i m jednosmjernih cesta. Ispiši neku stazu kojom 
+ralica može obići svaku cestu točno jednom. Takva staza će postojati. 
+U prvom redu ulaznih podataka se nalaze dva broja n (1 <= n <= 1000) 
+i m (1 <= m <= n*n). Zatim se nalazi m puta po dva broja a i b 
+(0 <= a,b < n) koja označavaju da su gradovi a i b povezani cestom od 
+a prema b.
+
+output:
+2 3 6 2 5 3 1 4 6 1 0
+
+*/
