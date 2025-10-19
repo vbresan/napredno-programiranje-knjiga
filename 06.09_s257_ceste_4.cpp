59c4eb366df85f9d5586df792934731080a6b007
Viktor Bresan <288855+vbresan@users.noreply.github.com>
2025-10-19 15:52:33 +0200

    Kruskal, 2st version

--- 06.09_s257_ceste_4.cpp
+++ 06.09_s257_ceste_4.cpp
@@ -11,54 +11,61 @@ Implementacija Kruskalovog algoritma.
 #include <numeric>
 using namespace std;
 
+int find(int node, vector<int>& trees) {
+
+  if (trees[node] != node) {
+    trees[node] = find(trees[node], trees);
+  }
+  
+  return trees[node];
+}
+
 int main() {
 
   int n, m;
   cin >> n >> m;
 
-  set<pair<int, pair<int, int>>> edges;
+  vector<tuple<int, int, int>> edges;
   for (int i = 0; i < m; ++i) {
     int a, b, c;
     cin >> a >> b >> c;
-    edges.emplace(c, make_pair(a, b));
+    edges.emplace_back(c, a, b);
   }
+  sort(edges.begin(), edges.end());
 
-  vector<int> count(n, 1);
+  vector<int> sizes(n, 1);
   vector<int> trees(n);
   iota(trees.begin(), trees.end(), 0);
   
   int total_distance = 0;
-  for (int i = 0; i < n - 1; ++i) {
+  int edges_added = 0;
+  for (auto [distance, from, to] : edges) {
 
-    const auto [distance, nodes] = *edges.begin();
-    auto [from_node, to_node] = nodes;
-    edges.erase(edges.begin());
+    cout << "Node From: " << char('A' + from) << " To: " << char('A' + to) << "\t\t";
 
-    cout << "Node From: " << char('A' + from_node) << " To: " << char('A' + to_node) << "\t\t";
+    from = find(from, trees);
+    to = find(to, trees);
 
-    while (trees[from_node] != from_node) {
-      from_node = trees[from_node];
-    }
-    while (trees[to_node] != to_node) {
-      to_node = trees[to_node];
-    }
-    
-    cout << "Tree From: " << char('A' + trees[from_node]) << " To: " << char('A' + trees[to_node]);
+    cout << "Tree From: " << char('A' + from) << " To: " << char('A' + to);
 
-    if (trees[from_node] == trees[to_node]) {
+    if (from == to) {
       cout << " (skipping)" << endl;
-      --i;
       continue;
     }
     cout << endl;
 
-    total_distance += distance;
-    
-    const int smaller = (count[from_node] < count[to_node])? from_node : to_node;
-    const int bigger = (count[from_node] >= count[to_node])? from_node : to_node;
+    const auto [smaller, bigger] = (sizes[from] < sizes[to])?
+      make_pair(from, to) : make_pair(to, from);
 
-    trees[smaller] = trees[bigger];
-    count[bigger] += count[smaller];
+    trees[smaller] = bigger;
+    sizes[bigger] += sizes[smaller];
+
+    total_distance += distance;
+    ++edges_added;
+
+    if (edges_added == n - 1) {
+      break;
+    }
   }
 
   cout << total_distance << endl;
