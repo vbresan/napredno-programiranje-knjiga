aa8316bbed8cd9f71e7310ca18426ed92cc79b3e
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-11-13 20:03:36 +0100

    Solution implementation

--- 06.10.01_s260_mis.cpp
+++ 06.10.01_s260_mis.cpp
@@ -12,19 +12,49 @@ Output: 256
 
 */
 
+#include <algorithm>
 #include <iostream>
+#include <utility>
+#include <vector>
 using namespace std;
+
+vector<vector<pair<int, int>>> tree;
+
+long long dfs(int parent, int node) {
+
+  long long maxDistance = 0;
+
+  for (int i = 0, size = tree[node].size(); i < size; ++i) {
+    if (tree[node][i].first != parent) {
+      maxDistance = max(maxDistance,
+                        dfs(node, tree[node][i].first) + tree[node][i].second);
+    }
+  }
+
+  return maxDistance;
+}
 
 int main() {
 
   int n, x;
   cin >> n >> x;
+  tree.insert(tree.begin(), n, vector<pair<int, int>>());
 
+  long long edgeSum = 0;
   for (int i = 0; i < n - 1; ++i) {
 
     int a, b, c;
     cin >> a >> b >> c;
+    --a;
+    --b;
+
+    tree[a].push_back(make_pair(b, c));
+    tree[b].push_back(make_pair(a, c));
+
+    edgeSum += c;
   }
+
+  cout << 2 * edgeSum - dfs(-1, x - 1) << endl;
 
   return 0;
 }
