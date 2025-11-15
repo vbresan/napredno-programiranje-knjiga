726da308d0f576bb9eec291ee82178cfa5e11cc1
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-11-15 08:25:33 +0100

    Solving the problem

--- 06.10.02_s262_mis.cpp
+++ 06.10.02_s262_mis.cpp
@@ -15,3 +15,71 @@ bi pojeo sve sireve.
 Output: 225
 
 */
+
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef vector<vector<pair<int, int>>> Tree;
+
+long long longest = 0;
+
+int dfs(const Tree &tree, int distance, int parent, int node) {
+
+  long long longest1 = 0, longest2 = 0, current = 0;
+
+  for (const auto &[neighbor, weight] : tree[node]) {
+    if (neighbor != parent) {
+      current = dfs(tree, distance + weight, node, neighbor) + weight;
+
+      if (current >= longest1) {
+        longest2 = longest1;
+        longest1 = current;
+      } else if (current > longest2) {
+        longest2 = current;
+      }
+    }
+  }
+
+  if (longest1 + longest2 > longest) {
+    longest = longest1 + longest2;
+  }
+  if (longest1 + distance > longest) {
+    longest = longest1 + distance;
+  }
+
+  return longest1;
+}
+
+int main() {
+
+  int n;
+  cin >> n;
+  Tree tree(n);
+
+  long long edgesSum = 0;
+  for (int i = 0; i < n - 1; ++i) {
+
+    int a, b, c;
+    cin >> a >> b >> c;
+    --a;
+    --b;
+
+    tree[a].emplace_back(b, c);
+    tree[b].emplace_back(a, c);
+    edgesSum += c;
+  }
+
+  for (int i = 0; i < n; ++i) {
+    if (tree[i].size() == 1) {
+      dfs(tree, 0, -1, i);
+      break;
+    }
+  }
+
+  cout << 2 * edgesSum - longest << endl;
+
+  return 0;
+}
