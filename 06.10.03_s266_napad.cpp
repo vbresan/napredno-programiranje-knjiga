01ab5eee7fb2d2165fcd03edf3bb3b15ccac3ce0
Viktor Bresan <288855+vbresan@users.noreply.github.com>
2025-11-15 18:13:57 +0100

    Adding new problem description and input

---
+++ 06.10.03_s266_napad.cpp
@@ -0,0 +1,11 @@
+/*
+Neprijatelj je napao državu s n (1 <= n <= 100000) gradova i 
+m (n-1 <= m <= 1000000) cesta između gradova. Gradovi su međusobno povezani. 
+Grad je "bitan" ako bi njegovim osvajanjem neprijatelj razdvojio ostale 
+gradove. Ispiši sve "bitne" gradove poredane po veličini. Ulazni podaci su 
+n i m, zatim m puta po dva broja a i b (0 <= a,b <= n-1) koji predstavljaju 
+dva 0-indeksirana grada povezana cestom.
+
+Output: 2 5 6 8
+
+*/
