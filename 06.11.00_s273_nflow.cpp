f7d1ad5dc8eb53b55b379e21f667df5b1644990a
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-11-17 09:53:46 +0100

    Adding new problem description and input

---
+++ 06.11.00_s273_nflow.cpp
@@ -0,0 +1,8 @@
+/*
+Zadani su n, m, x i y ,zatim m puta tri broja a, b i c. U jednostavnom
+povezanom usmjerenom grafu s m bridova i n čvorova, pronađi i ispiši Network
+flow od čvora x do čvora y. Brojevi a, b i c označavaju da od čvora a do
+čvora b postoji usmjeren brid težine c. Čvorovi su 0-indeksirani.
+
+Ispis: 87
+*/
