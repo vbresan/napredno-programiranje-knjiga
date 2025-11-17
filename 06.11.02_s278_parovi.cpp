7060f80301c166413bfb02e8ea516e7178513a57
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-11-17 18:57:50 +0100

    Adding new problem description and input

---
+++ 06.11.02_s278_parovi.cpp
@@ -0,0 +1,16 @@
+/*
+Unosi se n1 (broj čvorova iz prvog skupa), n2 (broj čvorova iz drugog skupa)
+i m, zatim m bridova opisanih s po dva broja a i b. Čvor a (0 < = a < n) iz
+prvog skupa je povezan bridom s čvorom b (0 < = b < n2) iz drugog skupa.
+Ispiši koliko najviše parova čvorova se može povezati (nekim od postojećih
+bridova) tako da je svaki čvor povezan s najviše jednim čvorom.
+
+Odgovarajuci ispis:
+5
+  Slijedi dodani ispis:
+A + 4
+B + 2
+C + 3
+D + 1
+E + 5
+*/
