ef390231c9d8579fc11888f8c6255405c6d47063
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-11-17 20:23:02 +0100

    Solving the problem

--- 06.11.02_s278_parovi.cpp
+++ 06.11.02_s278_parovi.cpp
@@ -14,3 +14,54 @@ C + 3
 D + 1
 E + 5
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> graph;
+vector<bool> visited;
+vector<int> pairs;
+
+int dfs(int node) {
+
+  visited[node] = true;
+  for (int neighbor : graph[node]) {
+    if (pairs[neighbor] == -1 ||
+        (!visited[pairs[neighbor]] && dfs(pairs[neighbor]))) {
+      pairs[neighbor] = node;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int main() {
+
+  int n1, n2, m;
+  cin >> n1 >> n2 >> m;
+
+  graph.resize(n1);
+  pairs.resize(n2, -1);
+
+  for (int i = 0; i < m; ++i) {
+
+    int a, b;
+    cin >> a >> b;
+    graph[a].push_back(b);
+  }
+
+  int paired = 0;
+  for (int i = 0; i < n1; ++i) {
+    visited = vector<bool>(n1);
+    paired += dfs(i);
+  }
+
+  cout << paired << endl;
+  cout << "  Slijedi dodani ispis:" << endl;
+  for (int i = 0; i < n2; ++i) {
+    cout << (char)(i + 'A') << " + " << pairs[i] + 1 << endl;
+  }
+
+  return 0;
+}
