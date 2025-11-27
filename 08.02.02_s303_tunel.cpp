3a97c51fba3de11842dcceb934b1bc77688e889d
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-11-27 21:05:43 +0100

    Adding new problem description and input

---
+++ 08.02.02_s303_tunel.cpp
@@ -0,0 +1,18 @@
+/*
+Nalazite se u dugačkom tunelu koji ima pet traka za automobile. Na ulazu u
+tunel nalazi se vaš prijatelj koji vam je dojavio kojim će trakama dolaziti
+automobili tijekom sljedećih n (1 <= n <= 1000000) sekundi. U tunelu nema
+mjesta kako bi se maknuli s ceste. Želite biti na onim trakama kojima ne voze
+automobili kako vas ne bi pregazili. Ako npr. u sedmoj sekundi prvom, drugom,
+trećom i petom trakom prolazi automobil, vi želite u sedmoj sekundi biti na
+četvrtoj traci, kako vas ne bi pregazio automobil. Problem je u tome što vam
+za prelaženje na susjednu traku treba jedna sekunda. Ako ste u x-toj sekundi
+na drugoj traci, onda u (x+1)-oj sekundi možete biti na prvoj, drugoj ili
+trećoj traci. Prije nego dođe prvi automobil, možete biti u bilo kojoj traci.
+Ulazni podaci su n i zatim n puta po pet znakova koji mogu biti '-' ili 'x'.
+Ako u i-tom retku unosa piše '---xx', to znači da u i-toj sekundi četvrtom i
+petom trakom prolazi automobil. Na početku se nalazite na bilo kojoj traci.
+Ispiši koliko minimalno puta morate biti pregaženi.
+
+Ispis: 2
+*/
