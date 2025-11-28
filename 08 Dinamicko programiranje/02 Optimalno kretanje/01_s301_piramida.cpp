5783eea23966eab1cafdb082da7f44c262eb005c
Viktor Bresan <288855+vbresan@users.noreply.github.com>
2025-11-28 16:13:29 +0100

    Algorithm optimizations

--- 08 Dinamicko programiranje/02 Optimalno kretanje/01_s301_piramida.cpp
+++ 08 Dinamicko programiranje/02 Optimalno kretanje/01_s301_piramida.cpp
@@ -1,9 +1,15 @@
 /*
-Unosi se n (1 <=n <= 1000). U sljedećih n redova nalaze se pozitivni cijeli
+Unosi se n (1 <= n <= 1000). U sljedećih n redova nalaze se pozitivni cijeli
 brojevi tako da se u x-tom retku nalazi x brojeva. Cilj je u toj "piramidi"
 brojeva pronaći put od vrha do dna, kojemu je suma brojeva najveća i ispisati
 taj put. Počinjemo u prvom retku (vrh piramide) i možemo se kretati samo
 prema dolje ili dolje-desno.
+
+Ispis 1:
+25 10 15 30 27
+
+Ispis 2:
+1 3 6 10
 */
 
 #include <algorithm>
@@ -25,8 +31,12 @@ int main() {
   int n;
   cin >> n;
 
-  vector<vector<int>> input(n);
+  vector<vector<int>> input(n), dp(n);
   for (int i = 0; i < n; ++i) {
+    
+    // 0-padding to avoid vector indexing problems and keep the algo. simple.
+    dp[i].resize(i + 3, 0); 
+    
     for (int j = 0; j <= i; ++j) {
       int x;
       cin >> x;
@@ -34,30 +44,27 @@ int main() {
     }
   }
 
-  vector<vector<int>> dp = input;
-  for (int i = 0; i < n; ++i) {
-    /* add leading and trailing zeros to avoid vector indexing problems
-       and keep the algorithm simple. */
-    dp[i].insert(dp[i].begin(), 0);
-    dp[i].push_back(0);
-  }
-
   // calculate optimum values
+  dp[0][1] = input[0][0];
   for (int i = 1; i < n; ++i) {
     for (int j = 1; j <= i + 1; ++j) {
-      dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + dp[i][j];
+      dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + input[i][j - 1];
     }
   }
 
   // backtrack the optimum path
   vector<int> path;
-  for (int i = n - 1, indexMax = dp[i].size() - 1; i >= 0; --i) {
-    Iterator last = dp[i].begin() + min(indexMax, (int)(dp[i].size() - 1)) + 1;
-    indexMax = getIndexMax(dp[i].begin(), last);
-    path.insert(path.begin(), input[i][indexMax - 1]);
+  int column = getIndexMax(dp[n - 1].begin(), dp[n - 1].end() - 1);
+  for (int row = n - 1; row > 0; --row) {
+    path.push_back(input[row][column - 1]);
+    if (dp[row - 1][column - 1] > dp[row - 1][column]) {
+      column -= 1;
+    }
   }
+  path.push_back(input[0][0]);
 
   // print the path
+  reverse(path.begin(), path.end());
   for (int element : path) {
     cout << element << ' ';
   }
