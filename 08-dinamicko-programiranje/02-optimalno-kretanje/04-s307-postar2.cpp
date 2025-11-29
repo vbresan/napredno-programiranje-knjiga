7ad56525e64fddd04f7366af5c8699061d88d39a
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-11-29 18:05:11 +0100

    Adding new problem description

---
+++ 08-dinamicko-programiranje/02-optimalno-kretanje/04-s307-postar2.cpp
@@ -0,0 +1,13 @@
+/*
+Poštanski ured ima posebnu prostoriju oblika matrice n x m. Na svakom polju
+te matrice nalazi se od 0 do 9 pisama koja treba raznijeti. Prije nego krene
+raznositi pisma, poštar uđe u opisanu prostoriju s namjerom da uzme što više
+pisama obilazeći prostoriju (matricu) triput. Prvi se put kreće od gornjeg
+lijevog kuta do donjeg desnog kuta, isključivo prema dolje ili desno. Nakon
+toga se kreće obrnuto, tj. od donjeg desnog kuta do gornjeg lijevog
+isključivo prema gor€ ili lijevo. Nakon toga se ponovno kreće od gornjeg
+lijevog kuta prema donjem desnom isključivo prema dolje ili desno. Ista se
+pisma ne mogu pokupiti dvaput. Koliko najviše pisama poštar može uzeti?
+Ulazni podaci su n i m (3 <= n,m <= 50), zatim n redova koji sadrže niz od m
+brojeva t (0 <=t <= 9).
+*/
