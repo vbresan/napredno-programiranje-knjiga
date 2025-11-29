a811bdeadacbf09a3a256cbccec814db00e79db9
Viktor Bresan <288855+vbresan@users.noreply.github.com>
2025-11-29 17:56:21 +0100

    Solving the problem

--- 08-dinamicko-programiranje/02-optimalno-kretanje/03-s305-postar.cpp
+++ 08-dinamicko-programiranje/02-optimalno-kretanje/03-s305-postar.cpp
@@ -10,3 +10,42 @@ koji sadrze niz od m brojeva x (0 <= x <= 9).
 
 Ispis: 98
 */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int toint(char c) {
+  return c - '0';
+}
+
+int main() {
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<int> row(m);
+  string s;
+  cin >> s;
+
+  row[0] = toint(s[0]);
+  for (int i = 1; i < m; ++i) {
+    row[i] = row[i - 1] + toint(s[i]);
+  }
+
+  for (int i = 1; i < n; ++i) {
+    
+    cin >> s;
+
+    vector<int> previous = row;
+    row[0] = previous[0] + toint(s[0]);
+    for (int j = 1; j < m; ++j) {
+      row[j] = max(row[j - 1], previous[j]) + toint(s[j]);
+    }
+  }
+
+  cout << row[m - 1] << endl;
+
+  return 0;
+}
