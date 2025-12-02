162bd0eb4f63fad366dfeb766779f160c37a4972
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-02 10:57:02 +0100

    Adding new problem description and input

---
+++ 08-dinamicko-programiranje/02-optimalno-kretanje/05-s310-vatra.cpp
@@ -0,0 +1,16 @@
+/*
+Vatra je zahvatila n (2 <=n <= 1000) drveca koja su poredana po pravcu.
+Drveca su različito vrijedna. Svaki sat vatra napravi na određenom drvetu x
+štete. Vatrogasci su kod m-tog drveta i moraju ugasiti vatru uz što manju
+štetu (prvo gase vatru kod m-tog drveta). Pritom vatrogasci ne mogu proci
+pored drveta, a da ne ugase vatru (u bilo kojem trenutku sva ugašena drveća
+čine neprekinuti niz drveća). Vatrogascima treba jedan sat da ugase vatru (ne
+troše vrijeme prilikom kretanja između drveća). Šteta nastaje sve dok
+vatrogasci ne ugase drvo do kraja (znači i tijekom sat vremena gašenja).
+Ulazni podacisu n i m (0<= m < n). m je 0-indeksiran redni broj drveta kod
+kojeg su na početku vatrogasci. Zatim slijedi n cijelih brojeva x
+(1 <= x <= 1000) koji redom opisuju drveće poredano po pravcu. Ispiši
+minimalnu štetu koja će nastati pri gašenju svih drveća.
+
+Ispis: 393
+*/
