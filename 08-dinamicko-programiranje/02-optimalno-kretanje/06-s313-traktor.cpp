cc8e251b2117e3fefb5a6490ad6ae2b867c8e677
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-05 07:05:26 +0100

    Adding new problem description and input

---
+++ 08-dinamicko-programiranje/02-optimalno-kretanje/06-s313-traktor.cpp
@@ -0,0 +1,19 @@
+/*
+U cijelom selu jedino Domagoj ima traktor. Domagoj iznajmljuje traktor.
+Seljani su dali Domagoju svoje zamolbe za iznajmljivanje traktora. Ukupno ima
+n zamolbi i svaka se zamolba sastoji od dvaju brojeva a i b, što znači da neki
+seljak želi unajmiti traktor na dan a vratio bi ga na dan b (na dan b se
+traktor ponovno može unajmiti). Domagoj želi traktor iznajmljivati na takav
+način da što više dana bude iznajmljen, kako bi što više zaradio i kako bi on
+morao što manje raditi. (Poznato je da se s traktorom mora stalno raditi pa
+kad traktor nije iznajmljen, Domagoj mora s njime u polje). Ponekad se događa
+da više zamolbi za unajmljivanje traktora počinju na isti dan, pa Domagoj mora
+odabrati kome iznajmiti traktor. Ako je traktor slobodan na dan x i postoji
+zamolba za unajmljivanje traktora od dana x, onda Domagoj mora iznajmiti
+traktor (kako se seljani ne bi ljutili na njega). Ulazni podaci su n
+(1 <=n <= 1000000) i m (1 <= m <= 1000000) i zatim n puta po dva broja a i b
+koji opisuju pojedinu zamolbu (0 <= a < b <= m). Ispiši koliko najviše dana
+traktor može biti iznajmljen.
+
+Ispis: 11
+*/
