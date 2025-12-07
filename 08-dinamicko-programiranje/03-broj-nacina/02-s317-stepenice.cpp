85692ab9ff6f926753e6ab5855ab0cf759b57034
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-07 17:58:18 +0100

    Adding new problem description

---
+++ 08-dinamicko-programiranje/03-broj-nacina/02-s317-stepenice.cpp
@@ -0,0 +1,12 @@
+/*
+Stubište se sastoji od n stuba. Na koliko se načina možemo popeti od dna do
+vrha stubišta ako svaki put možemo birati želimo li zakoračiti po jednu ili
+dvije stube. Na pocetku se nalazimo ispred prve stube. Ulazni podatak je n
+(2 <= n <= 1000000). Treba ispisati ostatak pri dijeljenju broja načina
+penjanja uz stube s 1000000 (zato što broj nacina penjanja može biti vrlo
+velik).
+
+Unos: 15
+Ispis: 987
+
+*/
