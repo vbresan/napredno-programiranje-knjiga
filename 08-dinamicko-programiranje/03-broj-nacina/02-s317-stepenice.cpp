9bae64338689ab01e6c23339d7fda7533e75d8ca
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-07 18:07:31 +0100

    Solving the problem

--- 08-dinamicko-programiranje/03-broj-nacina/02-s317-stepenice.cpp
+++ 08-dinamicko-programiranje/03-broj-nacina/02-s317-stepenice.cpp
@@ -10,3 +10,22 @@ Unos: 15
 Ispis: 987
 
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int n;
+  cin >> n;
+
+  int dp0, dp1 = 1, dp2 = 2;
+  for (int i = 3; i <= n; ++i) {
+    dp0 = dp1;
+    dp1 = dp2;
+    dp2 = (dp1 + dp0) % 1000000;
+  }
+
+  cout << dp2 << endl;
+  return 0;
+}
