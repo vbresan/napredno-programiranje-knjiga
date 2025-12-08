ca302a2b31b2a7771d438e03c0809dbeac4a7ac8
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-08 14:33:32 +0100

    Adding problem description and input

---
+++ 08-dinamicko-programiranje/03-broj-nacina/03-s318-zec.cpp
@@ -0,0 +1,15 @@
+/*
+Zec trči kroz šumu pravokutnog oblika. Šuma ima prohodna polja i polja na
+kojima je kamen. Zec se ne može kretati poljima na kojima je kamen. Na koliko
+načina zec može doći iz gornjeg lijevog kuta šume do donjeg desnog, ako se
+kreće isključivo prema dolje ili desno. Ulazni podaci su n i m
+(1 <= n,m <=1000), zatim n redova od po m znakova '.' ili 'x'. '.' je
+prohodno polje, a 'x' je kamen. Ispišite ostatak pri dijeljenju broja načina
+na koji zec može doći od gornjeg lijevog polja šume do donjeg desnog,
+s 1000000.
+
+Napomene: Moguće je da je broj načina 0, zbog kamenja. Gornje lijevo polje i
+donje desno će uvijek biti prohodno.
+
+Ispis: 8
+*/
