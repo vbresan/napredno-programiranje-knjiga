0818d6b98df5dc03d802a8565605513f0ef1dca3
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-15 19:16:15 +0100

    Adding new problem description and input

---
+++ 08-dinamicko-programiranje/04-podnizovi/01-s326-suma.cpp
@@ -0,0 +1,10 @@
+/*
+Nogometni klub vodi evidenciju o financijskim dobicima i gubicima. Svaki
+tjedan zabilježi se neki broj x (-1000 <= x <= 1000) koji predstavlja dobitak
+ili gubitak ostvaren tijekom prošlog tjedna. Upravu kluba zanima koliko je
+klub ikad najviše zaradio u nekom vremenskom intervalu. Ulazni podaci su n
+(1 <= n <= 1000000) i zatim n brojeva x (barem će jedan x biti pozitivan).
+Ispišite najveću sumu nekih uzastopnih brojeva u unesenom nizu.
+
+Ispis: 21
+*/
