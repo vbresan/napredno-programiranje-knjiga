d8e36061cac1b4dcda7d859f42be160b51295f4d
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-15 18:34:46 +0100

    Adding new problem descriptio and input

---
+++ 08-dinamicko-programiranje/03-broj-nacina/07-s324-podjela.cpp
@@ -0,0 +1,6 @@
+/*
+Na koliko načina zadane brojeve možemo podijeliti u dvije grupe tako da je
+zbroj brojeva u pojedinoj grupi jednak? Ulazni podaci su n (2 <= n <= 100) i
+zatim n cijelih, međusobno različitih, brojeva a (1 <= a <= 100). Ispišite
+ostatak pri dijeljenju broja načina s 999907.
+*/
