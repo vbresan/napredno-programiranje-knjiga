acea1ba9dacca2d09b701a0991170fce703e1128
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-20 10:38:25 +0100

    File rename. Adding new problem description and input

---
+++ 08-dinamicko-programiranje/04-podnizovi/03-s331-zajednicki.cpp
@@ -0,0 +1,10 @@
+/*
+Mirela radi za tajnu organizaciju i mora uočiti pravilnost između dva niza
+podataka. Cilj joj je u dvama nizovima brojeva pronaći dužinu najdužeg (ne
+obavezno povezanog) podniza koji se pojavljuje u oba niza. Ulazni podaci su
+n i m (1 <= n,m <= 1000). n je dužina prvog niza brojeva, a m duzina drugog
+niza brojeva. Nakon toga slijedi n cijelih brojeva i zatim m cijelih brojeva.
+Ispisite duzinu najduzeg zajednickog podniza.
+
+Ispis: 5
+*/
