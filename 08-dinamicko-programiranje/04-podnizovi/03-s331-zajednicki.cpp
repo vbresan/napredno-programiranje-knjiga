987590b260becc8eb97f478bc4802f4649d70d7c
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-21 18:03:08 +0100

    Solving the problem

--- 08-dinamicko-programiranje/04-podnizovi/03-s331-zajednicki.cpp
+++ 08-dinamicko-programiranje/04-podnizovi/03-s331-zajednicki.cpp
@@ -8,3 +8,44 @@ Ispisite duzinu najduzeg zajednickog podniza.
 
 Ispis: 5
 */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<int> a(n);
+  for (int &number : a) {
+    cin >> number;
+  }
+
+  vector<int> b(m);
+  for (int &number : b) {
+    cin >> number;
+  }
+
+  vector<vector<int>> dp(n, vector<int>(m));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+
+      if (i) {
+        dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+      }
+      if (j) {
+        dp[i][j] = max(dp[i][j], dp[i][j - 1]);
+      }
+      if (a[i] == b[j]) {
+        dp[i][j] = max(dp[i][j], ((i && j) ? dp[i - 1][j - 1] : 0) + 1);
+      }
+    }
+  }
+
+  cout << dp[n - 1][m - 1] << endl;
+
+  return 0;
+}
