8fd9f76db3c0861d11df99c870b1c5de8ae965d1
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-23 11:31:25 +0100

    Renaming variables for clarity

--- 08-dinamicko-programiranje/03-broj-nacina/07-s324-podjela.cpp
+++ 08-dinamicko-programiranje/03-broj-nacina/07-s324-podjela.cpp
@@ -4,23 +4,23 @@ zbroj brojeva u pojedinoj grupi jednak? Ulazni podaci su n (2 <= n <= 100) i
 zatim n cijelih, međusobno različitih, brojeva a (1 <= a <= 100). Ispišite
 ostatak pri dijeljenju broja načina s 999907.
 
-Ispis: 7
+Ispis: 9
 */
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-const int mod = 999907;
+const int MOD = 999907;
 
-long long pow(long long base, int n, int x) { // (base ^ n) % x
+long long mod_pow(long long base, int exp, int mod) { // (base ^ exp) % mod
   long long result = 1;
-  while (n) {
-    if (n & 1) {
-      result = (result * base) % x;
+  while (exp) {
+    if (exp & 1) {
+      result = (result * base) % mod;
     }
-    base = (base * base) % x;
-    n >>= 1;
+    base = (base * base) % mod;
+    exp >>= 1;
   }
   return result;
 }
@@ -48,11 +48,11 @@ int main() {
 
   for (const int number : numbers) {
     for (int i = sum - number; i >= 0; --i) {
-      dp[i + number] = (dp[i + number] + dp[i]) % mod;
+      dp[i + number] = (dp[i + number] + dp[i]) % MOD;
     }
   }
 
-  cout << (dp[sum] * pow(2, mod - 2, mod)) % mod << endl;
+  cout << (dp[sum] * mod_pow(2, MOD - 2, MOD)) % MOD << endl;
 
   return 0;
 }
