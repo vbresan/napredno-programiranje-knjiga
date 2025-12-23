54fdfd4751ac27fcded227ab3b55c3734c097984
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-23 18:26:11 +0100

    New problem description and input

---
+++ 08-dinamicko-programiranje/05-knapsack/s332-kamion.cpp
@@ -0,0 +1,11 @@
+/*
+U kamion se tovari pijesak. U kamion stane x kubika pijeska. Ukupno
+raspolažemo s n vrsta pijeska. Ulazni podaci su n (1 <=n <= 100000) i x,
+zatim n parova brojeva a i b koji opisuju pojedinu vrstu pijeska. a je
+količina pijeska u kubicima kojom raspolažemo, dok je b ukupna vrijednost
+tih a kubika pijeska. Kamion valja natovariti tako da je ukupna vrijednost
+pijeska u njemu što veća. Od svake vrste pijeska možemo uzeti onoliko kubika
+koliko želimo (ne moramo uzeti svih a).
+
+Ispis: 175
+*/
