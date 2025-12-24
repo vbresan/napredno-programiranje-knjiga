e0dc49640d92bbf0efb0ee5922988f26cc66ee6e
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-24 10:03:50 +0100

    Solving the problem

--- 08-dinamicko-programiranje/05-knapsack/s332-kamion.cpp
+++ 08-dinamicko-programiranje/05-knapsack/s332-kamion.cpp
@@ -9,3 +9,42 @@ koliko želimo (ne moramo uzeti svih a).
 
 Ispis: 175
 */
+
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int main() {
+
+  int n, x;
+  cin >> n >> x;
+
+  vector<pair<double, int>> pairs;
+  for (int i = 0; i < n; ++i) {
+
+    int a, b;
+    cin >> a >> b;
+
+    pairs.emplace_back((double)b / a, a);
+  }
+
+  sort(pairs.begin(), pairs.end());
+
+  int totalValue = 0, remainingCapacity = x;
+  while (remainingCapacity) {
+
+    const auto &[itemPrice, itemAmount] = pairs.back();
+    int currentAmount = min(remainingCapacity, itemAmount);
+
+    totalValue += currentAmount * itemPrice;
+    remainingCapacity -= currentAmount;
+
+    pairs.pop_back();
+  }
+
+  cout << totalValue << endl;
+
+  return 0;
+}
