1fed241906981a8a30aa8055ee651b5351416b62
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-25 10:38:16 +0100

    Adding new problem description and input file

---
+++ 08-dinamicko-programiranje/06-trazenje-kvadrata/02-s338-np.cpp
@@ -0,0 +1,17 @@
+/*
+Država pravokutnog oblika dimenzije n x n je za potrebe proglašavanja
+nacionalnog parka odlučila analizirati svoj teritorij. Ulazni podaci su
+n (1 <= n <= 1000) i zatim n linija s po n znakova koji opisuju izgled države.
+Svaki znak može predstavljati slobodan prostor '.', ili naseljen prostor '#'.
+Treba ispisati koliko različitih kvadratnih nenaseljenih područja svake
+veličine postoji. Za potrebu analize se pronađena kvadratna područja mogu
+preklapati. (Napomena: Pronađena kvadratna područja moraju biti orijentirana
+paralelno s rubovi- i ma države.)
+
+Ispis:
+Velicine 1 x 1 : 44
+Velicine 2 x 2 : 19
+Velicine 3 x 3 : 5
+Velicine 4 x 4 : 1
+
+*/
