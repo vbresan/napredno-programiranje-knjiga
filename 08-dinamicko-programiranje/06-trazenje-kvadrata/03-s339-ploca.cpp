234e0c50adbe52c4015e125b9a664faeda5ce088
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-25 17:35:23 +0100

    Solving the problem

--- 08-dinamicko-programiranje/06-trazenje-kvadrata/03-s339-ploca.cpp
+++ 08-dinamicko-programiranje/06-trazenje-kvadrata/03-s339-ploca.cpp
@@ -11,3 +11,46 @@ kvadratnog oblika.
 
 Ispis: 5
 */
+
+#include <iostream>
+#include <limits>
+#include <utility>
+#include <vector>
+using namespace std;
+
+constexpr int INF = numeric_limits<int>::max();
+
+inline void order(int &a, int &b) {
+  if (a > b) {
+    swap(a, b);
+  }
+}
+
+inline int get(const vector<vector<int>> &dp, int a, int b) {
+  order(a, b);
+  return dp[a][b];
+}
+
+int main() {
+
+  int n, m;
+  cin >> n >> m;
+  order(n, m);
+
+  vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+  for (int i = 1; i <= n; ++i) {
+    for (int j = i + 1; j <= m; ++j) {
+      dp[i][j] = INF;
+      for (int k = 1; k <= i / 2; ++k) {
+        dp[i][j] = min(dp[i][j], get(dp, k, j) + get(dp, i - k, j) + 1);
+      }
+      for (int k = 1; k <= j / 2; ++k) {
+        dp[i][j] = min(dp[i][j], get(dp, i, k) + get(dp, i, j - k) + 1);
+      }
+    }
+  }
+
+  cout << dp[n][m] << endl;
+
+  return 0;
+}
