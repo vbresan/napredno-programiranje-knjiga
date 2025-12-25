52b2ba0ab016707a6a6b10964291b6366f935f5e
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-25 10:58:51 +0100

    Solving the problem

--- 08-dinamicko-programiranje/06-trazenje-kvadrata/02-s338-np.cpp
+++ 08-dinamicko-programiranje/06-trazenje-kvadrata/02-s338-np.cpp
@@ -15,3 +15,50 @@ Velicine 3 x 3 : 5
 Velicine 4 x 4 : 1
 
 */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+inline int min3(int a, int b, int c) { return min(min(a, b), c); }
+
+inline int get(const vector<vector<int>> &dp, int x, int y) {
+  return (x < 0 || y < 0) ? 0 : dp[x][y];
+}
+
+int main() {
+
+  int n;
+  cin >> n;
+
+  vector<string> input(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> input[i];
+  }
+
+  vector<vector<int>> dp(n, vector<int>(n));
+  vector<int> result(n + 1);
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (input[i][j] == '.') {
+        dp[i][j] =
+            min3(get(dp, i - 1, j), get(dp, i, j - 1), get(dp, i - 1, j - 1)) +
+            1;
+        ++result[dp[i][j]];
+      }
+    }
+  }
+
+  for (int i = n - 1; i; --i) {
+    result[i] += result[i + 1];
+  }
+
+  for (int i = 1; i <= n; ++i) {
+    if (result[i]) {
+      cout << "Velicine " << i << " x " << i << " : " << result[i] << endl;
+    }
+  }
+
+  return 0;
+}
