65fa5cf622b5711235fdc8627ace3d4918089eca
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-25 18:10:29 +0100

    Adding new problem description and input

---
+++ 08-dinamicko-programiranje/07-rekurzije/01-s341-kantina.cpp
@@ -0,0 +1,22 @@
+/*
+Studentska kantina poslužuje pet vrsta jela studentima. Kuharica grabi
+studentima porcije hrane iz velikih posuda. Postoji pet posuda, svaka s
+odgovarajućom vrstom jela. Problem je što se ne mogu sva jela uvijek
+poslužiti, jer na pult stanu samo dvije posude. Kad student dođe do kuharice
+i kaže koje od pet jela želi jesti, ako jelo nije na pultu, kuharica mora
+maknuti jedno od jela s pulta, zatim ga odnijeti u kuhinju i vratiti se s
+posudom koja sadrži jelo koje student želi jesti i staviti ga na pult, kako bi
+mogla poslužiti traženo jelo. Svako od pet jela je različite težine, pa svako
+zahtijeva određeno vrijeme kako bi se prenijelo iz kuhinje na pult, ili s
+pulta u kuhinju. Kuharica je od Milana uspjela saznati kojim redom će studenti
+naručivati jelo i želi što brže poslužiti sve studente. Ulazni podaci se
+sastoje od pet brojeva t (1 <= t <= 1000), koji opisuju vrijeme u sekundama
+potrebno da se jelo stavi na pult ili makne s pulta. Nakon toga slijedi broj
+n (1 <= n <= 100000) i zatim n brojeva x (1 <= x <= 5) koji redom opisuju jela
+koje studenti žele jesti. Posluživanje jela koje je na pultu traje 10 sekundi.
+Ne mora se čekati na studente jer svi stoje u redu i čekaju na svoje porcije.
+Prije nego dođe prvi student, kuharica može staviti bilo koja dva jela na
+pult. Ispišite najmanji broj sekundi potreban da se posluže svi studenti.
+
+Ispis: 2000
+*/
