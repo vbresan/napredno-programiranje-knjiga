895a4d5b8a6b76715ed6045018b0366fab86d8cc
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-25 11:11:57 +0100

    Adding new problem description and input file

---
+++ 08-dinamicko-programiranje/06-trazenje-kvadrata/03-s339-ploca.cpp
@@ -0,0 +1,13 @@
+/*
+Ivan ima pilu i reze ploce. Kad dobije ploču dimenzija AxB, on je u minimalno
+poteza pilom razreže na ploče kvadratnog oblika. Svaki potez pilom paralelan
+je s rubovima ploče i rezanjem nastaju ploče cjelobrojnih dimenzija (ploča
+5x6 može se razrezati u ploče 2x6 i 3x6 ili npr. u 5x1 i 5x5, ali se ne može
+razrezati na 2.3x6 i 2.7x6). Razrezane ploče ne mogu se zajedno rezati, tj.
+reže se jedna po jedna (za rezanje ploče 4x4 na 4 ploče 2x2 treba 3 poteza
+pilom, a ne 2). Ulazni podaci su A i B (1 <= A,B <= 100). Ispišite koliko
+minimalno poteza pilom Ivan mora napraviti da bi ploču AxB razrezao na ploče
+kvadratnog oblika.
+
+Ispis: 5
+*/
