037add9420c2cdd70b0db253238b4e10cb1d2096
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-26 10:37:33 +0100

    Adding new problem description and input

---
+++ 08-dinamicko-programiranje/07-rekurzije/02-s344-led.cpp
@@ -0,0 +1,16 @@
+/*
+Kližemo se po velikoj ledenoj plohi dimenzije n puta m (1 <= n,m <= 1000).
+Svako polje te ledene plohe može biti prekriveno ledom ili velikim kamenom.
+Ledena ploha je okružena kamenjem, tako da ne možemo iskliznuti van polja. Kad
+stojimo na nekom polju (na kojem ne smije biti kamen), možemo se odgurnuti u
+nekom od četiri smjera: gore, dolje, lijevo i desno. Kad se odgurnemo, ne
+možemo se zaustaviti dok ne udarimo o neki kamen, koji nas zaustavi. Na
+početku se nalazimo u gornjem lijevom kutu plohe i zanima nas možemo li na
+nekom polju plohe stajati (ne gibati se). Ulazni podaci su n i m, te x
+(0 <= x < n) i y (0 <= y < m), Zatim slijedi n redova s po m znakova koji
+opisuju izgled plohe. Znak može biti '.' ili 'o'. '.' predstavlja led, a 'o'
+kamen. Treba Ispisati 1 ako se može stajati (ne gibati se) na lokaciji (x, y),
+a inace 0.
+
+Ispis: 1
+*/
