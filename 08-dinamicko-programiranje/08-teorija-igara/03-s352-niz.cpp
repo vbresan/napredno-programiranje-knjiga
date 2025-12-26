fb77f99430ff4156ff81282deb2d30b34753589e
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-26 16:07:05 +0100

    Adding new problem description and input

---
+++ 08-dinamicko-programiranje/08-teorija-igara/03-s352-niz.cpp
@@ -0,0 +1,11 @@
+/*
+Dan je niz od n (1 <= n <= 1000) brojeva s kojim se igra "niz". Željko i Katica
+naizmjenično uzimaju po jedan broj s početka ili kraja niza. Prvi igra Željko.
+Kad se uzmu sve brojke iz niza, igra završava i pobjeđuje onaj tko je uzeo
+veću sumu brojeva. Ako je izjednačeno pobjeđuje Katica. I Željko i Katica
+igraju optimalno. Ulazni podaci su n, zatim n brojeva manjih od 100000 koji
+opisuju niz s kojim se igra "niz". Ispišite "Zeljko" ako Željko pobjedi, a inače
+"Katica".
+
+Ispis: Zeljko
+*/
