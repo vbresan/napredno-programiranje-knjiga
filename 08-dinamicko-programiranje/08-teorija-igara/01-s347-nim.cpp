8fc4bbab1504f9d68ba54ddf8c1104782f6fb477
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-26 11:42:45 +0100

    Adding problem description

---
+++ 08-dinamicko-programiranje/08-teorija-igara/01-s347-nim.cpp
@@ -0,0 +1,7 @@
+/*
+Na hrpi se nalazi n (0 <= n <= 10^9) šibica. Dva igrača naizmjenično uzimaju
+jednu, dvije ili tri šibice s hrpe. Igru počinje igrač A, a zatim se
+izmjenjuju B, pa A, pa B, itd. Onaj tko ne moze uzeti niti jednu šibicu je
+izgubio. Ulazni podatak je n. Treba ispisati tko će pobijediti (A ili B) ako
+oba igrača igraju optimalno.
+*/
