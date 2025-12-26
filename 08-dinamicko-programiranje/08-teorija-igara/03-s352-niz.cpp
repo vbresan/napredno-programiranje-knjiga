90da7a4d80a9740ac35eb2a0b03618c6c1acde2e
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-26 17:32:00 +0100

    Solving the problem

--- 08-dinamicko-programiranje/08-teorija-igara/03-s352-niz.cpp
+++ 08-dinamicko-programiranje/08-teorija-igara/03-s352-niz.cpp
@@ -9,3 +9,36 @@ opisuju niz s kojim se igra "niz". Ispišite "Zeljko" ako Željko pobjedi, a ina
 
 Ispis: Zeljko
 */
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+  int n;
+  cin >> n;
+
+  vector<int> sequence(n);
+  for (int &number : sequence) {
+    cin >> number;
+  }
+
+  vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+  for (int i = 1; i <= n; ++i) {       // interval width
+    for (int j = 0; j <= n - i; ++j) { // interval start
+      if (i % 2 == n % 2) {            // Zeljko
+        dp[j][i] = max(dp[j][i - 1] + sequence[j + i - 1],
+                       dp[j + 1][i - 1] + sequence[j]);
+      } else { // Katica
+        dp[j][i] = min(dp[j][i - 1] - sequence[j + i - 1],
+                       dp[j + 1][i - 1] - sequence[j]);
+      }
+    }
+  }
+
+  cout << ((dp[0][n] > 0) ? "Zeljko" : "Katica") << endl;
+
+  return 0;
+}
