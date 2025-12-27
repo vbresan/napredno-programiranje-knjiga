80fa36cb3cb8b0d8d4cf4a4c0eaa1e12bd7a49ce
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-27 08:41:27 +0100

    Adding input

--- 09-ostalo/01-queue-s-maksimumom/s357-ulaznice.cpp
+++ 09-ostalo/01-queue-s-maksimumom/s357-ulaznice.cpp
@@ -7,4 +7,7 @@ najviša osoba u redu za kupnju ulaznica. Ulazni podaci sastoje se od broja n (1
 <= n <= 1000000), zatim n cijelih brojeva x koji opisuju pojedine događaje. Ako
 je x=0, prodana je još jedna karta, ako je x=-1 treba ispisati veličinu najviše
 osobe koja čeka u redu, a ako je x>0, na kraj reda je stala osoba visine x.
+
+Ispis:
+5 7 7 9 9 9 4
 */
