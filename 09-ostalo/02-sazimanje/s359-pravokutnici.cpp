fc9896dc3c89010e0c50c25d469e6a6fa5fdd26e
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-27 14:37:37 +0100

    Adding new chapter and problem description

---
+++ 09-ostalo/02-sazimanje/s359-pravokutnici.cpp
@@ -0,0 +1,9 @@
+/*
+Iz papira smo izrezali n pravokutnika i stavljamo ih na stol paralelno s
+rubovima stola. Ulazni podaci su n (1 <= n <= 100), zatim n puta po četiri
+prirodna broja Ax, Ay, Bx i By, manja od 1000000. Brojevi Ax, Ay, Bx i By
+predstavljaju gornju lijevu i donju desnu koordinatu koju će pravokutnik zauzeti
+na stolu. Pravokutnike stavljamo jedan preko drugog na stol, počevši od prvog
+unesenog. Ispišite redom za svaki pravokutnik koliki postotak njegove površine
+je vidljiv nakon što sve pravokutnike stavimo na stol.
+*/
