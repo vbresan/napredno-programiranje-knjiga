8d43680979ab080c98bab05e8785286e9bdd6bab
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-27 08:32:28 +0100

    New problem description

---
+++ 09-ostalo/01-queue-s-maksimumom/s357-ulaznice.cpp
@@ -0,0 +1,10 @@
+/*
+Ekipa kupuje ulaznice za popularni koncert. Radnja se odvija tako da pojedinac
+stane u red i ceka da svi ispred njega kupe ulaznice, tada i on kupi ulaznicu i
+napusti red. Koncert je medijski popraćen i ponekad se neki kamerman izdere:
+"Ej, ti visoki, sagni se!"" Zato je potrebn uvijek znati koliko je visoka
+najviša osoba u redu za kupnju ulaznica. Ulazni podaci sastoje se od broja n (1
+<= n <= 1000000), zatim n cijelih brojeva x koji opisuju pojedine događaje. Ako
+je x=0, prodana je još jedna karta, ako je x=-1 treba ispisati veličinu najviše
+osobe koja čeka u redu, a ako je x>0, na kraj reda je stala osoba visine x.
+*/
