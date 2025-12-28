d274a0e20881cd30fd965a1d1c4a600fc4085fb8
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-28 11:59:50 +0100

    Adding new chapter and problem description

---
+++ 09-ostalo/06-union-find/s370-puzzle.cpp
@@ -0,0 +1,11 @@
+/*
+Mirela se natječe u slaganju puzzli. Napredovanje tijekom natjecanja ocjenjuje
+komisija koja povremeno dođe do Mirele i projeri je li uspjela povezati neka
+dva dijela puzzle (direktno | ili preko nekih trećih dijelova puzzle). Puzzla
+koju treba složiti ima n (2 <= n <= 1000000) dijelova. Tijekom natjecanja događa
+se m (1 <= m <= 1000000) događaja. Ulazni podaci su n i m, zatim m parova
+brojeva x i y (0 <= x,y <= n). Ako je x<y, znači da je Mirela spojila dio puzle
+x s dijelom y. Ako je x>y, došla je komisija i treba ispisati '1' ako je Mirela
+povezala dio puzle x s dijelom y, a inače '0'. Napomena: Mirela može spojiti
+dijelove puzzle koji su već prije spojeni preko nekih drugih dijelova puzzle.
+*/
