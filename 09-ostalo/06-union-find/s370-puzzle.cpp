8ef32b0499143a034d712d9516f11e4eda724db9
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-28 14:38:09 +0100

    Adding input and solving the problem

--- 09-ostalo/06-union-find/s370-puzzle.cpp
+++ 09-ostalo/06-union-find/s370-puzzle.cpp
@@ -8,4 +8,54 @@ brojeva x i y (0 <= x,y <= n). Ako je x<y, znači da je Mirela spojila dio puzle
 x s dijelom y. Ako je x>y, došla je komisija i treba ispisati '1' ako je Mirela
 povezala dio puzle x s dijelom y, a inače '0'. Napomena: Mirela može spojiti
 dijelove puzzle koji su već prije spojeni preko nekih drugih dijelova puzzle.
+
+Ispis: 1010111
+
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int title(vector<int> &P, int x) {
+  return x == P[x] ? x : (P[x] = title(P, P[x]));
+}
+
+int main() {
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<int> P(n), D(n);
+  for (int i = 0; i < n; ++i) {
+    P[i] = i;
+  }
+
+  for (int i = 0; i < m; ++i) {
+    int a, b;
+    cin >> a >> b;
+
+    int Pa = title(P, a), Pb = title(P, b);
+    if (a > b) {
+      cout << (Pa == Pb);
+    } else {
+      if (Pa == Pb) {
+        continue;
+      }
+      if (D[Pa] > D[Pb]) {
+        P[Pb] = Pa;
+      }
+      if (D[Pa] < D[Pb]) {
+        P[Pa] = Pb;
+      }
+      if (D[Pa] == D[Pb]) {
+        P[Pb] = Pa;
+        ++D[Pa];
+      }
+    }
+  }
+
+  cout << endl;
+
+  return 0;
+}
