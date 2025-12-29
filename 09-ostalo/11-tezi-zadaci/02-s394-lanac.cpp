f849714dc663f064498f6a866f0c5ef0de9fe6f1
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2025-12-29 15:41:29 +0100

    Adding new problem description and input

---
+++ 09-ostalo/11-tezi-zadaci/02-s394-lanac.cpp
@@ -0,0 +1,16 @@
+/*
+Na olimpijadi starih sportova održava se natjecanje u nošenju lanca s utezima.
+Tim natjecatelja treba što prije prenijeti lanac s utezima od starta do cilja.
+Budući da se radi o lancu, svaki član tima uzme nekoliko utega koji su uzastopno
+postavljeni na lancu. Svaki uteg je kod nekog člana tima i cilj je što prije,
+krećući se zajedno, prenijeti utege do cilja. Svi članovi tima su jednako jaki i
+brzina nošenja utega ovisi o najopterećenijem članu tima, jer se on najsporije
+kreće. Zadan je broj utega na lancu n (1 <= n <= 100000) i broj članova tima m
+(1 <= m <= 10000), zatim n brojeva x (1 <= x <= 1000) koji predstavljaju težinu
+pojedinog utega. Utezi su poredani po lancu onim redom kojim su navedeni u
+unosu. Ispišite najveću težinu koju netko mora nositi ako se utezi rasporede
+optimalno (najveća težina koju netko nosi treba biti što manja, kako bi tim bio
+što brži).
+
+Ispis: 20
+*/
