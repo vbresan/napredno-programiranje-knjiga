4feb588da2effc5c2ff05b1c62e1525e9a5c3109
Viktor Brešan <288855+vbresan@users.noreply.github.com>
2026-01-01 16:05:48 +0100

    Minor problem description update

--- 09-ostalo/11-tezi-zadaci/03-s396-pravokutnik.cpp
+++ 09-ostalo/11-tezi-zadaci/03-s396-pravokutnik.cpp
@@ -1,7 +1,7 @@
 /*
 Zadan je tlocrt grada kvadratnog oblika dimenzije n^2. Ulazni podaci se sastoje
 od broja n (1 <= n <= 1000), zatim n linija s po n znakova. Svaki znak može biti
-'.' (slobodan prostor ili '#' (zauzet prostor). Ispišite površinu najvećeg
+'.' (slobodan prostor) ili '#' (zauzet prostor). Ispišite površinu najvećeg
 slobodnog pravokutnog (ne kvadratnog) prostora u gradu, čije su stranice
 paralelne s tlocrtom grada.
 
